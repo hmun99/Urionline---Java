b987f1e760829ff442a596cd4fabc63b225b360e
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-05-18 20:29:45 +0600

    Uri Online -1002
    Uri online -1002 solution , area of a circle

---
+++ UriOnline_3.java
@@ -0,0 +1,20 @@
+
+package urionline_3;
+
+import java.util.Scanner;
+
+public class UriOnline_3 {
+
+    
+    public static void main(String[] args) {
+       Scanner x = new Scanner(System.in);
+       double pi =  3.14159;
+       double a,b;
+       b = x.nextDouble();
+       a = pi*b*b;
+       System.out.printf("A=%.4f\n",a);
+       
+       
+    }
+    
+}

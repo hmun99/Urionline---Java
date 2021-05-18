913f1c1b34e69b06251daa7ece2c1db10d0c7eb8
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-05-18 20:28:34 +0600

    Uri Online_1001  Solution
    Uri online 1001 solution,
     extremely basic

---
+++ UriOnline_2.java
@@ -0,0 +1,22 @@
+
+
+package urionline_2;
+
+import java.util.Scanner;
+
+
+
+public class UriOnline_2 {
+
+    
+    
+    public static void main(String[] args) {
+        Scanner z = new Scanner(System.in);
+        int a = z.nextInt();
+        int b = z.nextInt();
+        int x = a+b;
+        System.out.println("X = "+x);
+        
+    }
+    
+}

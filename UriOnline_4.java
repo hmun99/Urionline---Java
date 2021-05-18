07c335b5922c79864975ca41a7481938118e4725
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-05-18 20:42:45 +0600

    Uri Online-1003
    Solution of Uri online-1003, Simple Sum

---
+++ UriOnline_4.java
@@ -0,0 +1,17 @@
+
+package urionline_4;
+
+import java.util.Scanner;
+
+
+public class UriOnline_4 {
+
+  
+    public static void main(String[] args) {
+       Scanner x = new Scanner(System.in);
+       int a = x.nextInt();
+       int b = x.nextInt();
+       int s = a+b;
+        System.out.println("SOMA = "+s);
+    }
+  }

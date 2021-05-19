b3270adcedf407ebfbd173b119a659a0b18addc7
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-05-19 16:17:51 +0600

    Uri Online-1004
    Solution of uri online -1004,simple product

---
+++ UriOnline_5.java
@@ -0,0 +1,22 @@
+
+
+package urionline_5;
+
+import java.util.Scanner;
+
+
+public class UriOnline_5 {
+
+   
+    
+    public static void main(String[] args) { 
+        Scanner x = new Scanner(System.in);
+       int a = x.nextInt();
+       int b = x.nextInt();
+       int s = a*b;
+        System.out.println("PROD = "+s);
+      
+       
+    }
+    
+}

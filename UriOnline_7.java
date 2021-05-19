ffb67ec58b22831ceb950e1cd1c629774b93a7fc
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-05-19 16:20:47 +0600

    Uri online -1006
    Solution of Uri online -1006, Average 2

---
+++ UriOnline_7.java
@@ -0,0 +1,21 @@
+
+package urionline_7;
+
+import java.util.Scanner;
+
+
+public class UriOnline_7 {
+
+   
+    public static void main(String[] args) {
+      Scanner x = new Scanner(System.in);
+      float  a = x.nextFloat();
+      float b = x.nextFloat();
+      float c = x.nextFloat();
+      double z;
+      z = (((a*2)+(b*3)+(c*5))/(2+3+5));
+      System.out.printf("MEDIA = %.1f\n",z);
+      
+    }
+    
+}

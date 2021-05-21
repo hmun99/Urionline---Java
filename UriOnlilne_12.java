83d177b9081dfb8b15cf58bf3364e8ad12ea746c
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-05-21 18:41:40 +0600

    Uri Online 1011
    Solution of uri online 1011, Sphere

---
+++ UriOnlilne_12.java
@@ -0,0 +1,20 @@
+
+package urionlilne_12;
+
+import java.util.Scanner;
+
+
+public class UriOnlilne_12 {
+
+    
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      double pi = 3.14159;
+      double r = sc.nextDouble();
+      double z ;
+      z = (4.0/3)*pi*r*r*r;
+      System.out.printf("VOLUME = %.3f\n",z);
+        
+    }
+    
+}

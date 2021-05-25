b39bd2e0ee40861791be80d0ae4db30c77993b84
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-05-25 21:35:51 +0600

    Uri Online 1014
    Solution of Uri online 1014, Consumption

---
+++ UriOnline_15.java
@@ -0,0 +1,19 @@
+
+package urionline_15;
+
+import java.util.Scanner;
+
+
+public class UriOnline_15 {
+
+  
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        float b = sc.nextFloat();
+        float c ;
+        c = a/b;
+        System.out.printf("%.3f km/l\n",c);
+    }
+    
+}

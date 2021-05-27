d63a070af898f7a65022cd20e8882dbc51b39cd0
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-05-28 01:11:04 +0600

    Uri Online 1017
    Solution of uri online 1017, Distance Between Two Points

---
+++ UriOnline_18.java
@@ -0,0 +1,21 @@
+
+package urionline_18;
+
+import java.util.Scanner;
+
+public class UriOnline_18 {
+
+  
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int x,y;
+        double z ;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        z = (x*y)/12.0;
+        System.out.printf("%.3f\n",z);
+    }
+    
+    
+}

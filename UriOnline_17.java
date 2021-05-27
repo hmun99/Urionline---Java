88a53e9cf9e60573c5ad8948b9efd3d3f5710791
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-05-28 01:08:23 +0600

    Uri Online 1016
    Solution of uri online 1016, Distance

---
+++ UriOnline_17.java
@@ -0,0 +1,19 @@
+
+package urionline_17;
+
+import java.util.Scanner;
+
+public class UriOnline_17 {
+
+    
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        int a,x;
+        a = sc.nextInt();
+        x = a*2;
+        System.out.printf("%d minutos\n",x);
+        
+    }
+    
+}

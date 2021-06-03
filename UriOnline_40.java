5b88cff42c6e40f783f0f4e1d0a1ce0e84ad8456
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-06-03 22:55:20 +0600

    Uri Online 1059,
    Solultion of Uri Online 1059, Even Numbers

---
+++ UriOnline_40.java
@@ -0,0 +1,19 @@
+
+package urionline_40;
+
+
+
+public class UriOnline_40 {
+
+   
+    public static void main(String[] args) {
+     
+     int i;
+    
+     for(i = 2;i <= 100; i++)
+         if(i%2 == 0){
+             System.out.print(i+"\n");
+         }
+    }
+    
+}

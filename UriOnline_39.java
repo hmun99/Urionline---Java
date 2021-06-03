ce93772cf79f1d2beab666300878fcdfcef6f903
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-06-03 22:52:43 +0600

    Uri Online 1051
    Solution Uri Online 1051,  Month

---
+++ UriOnline_39.java
@@ -0,0 +1,52 @@
+
+package urionline_39;
+
+import java.util.Scanner;
+
+public class UriOnline_39 {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a == 1){
+            System.out.printf("January\n");
+        }
+        else if(a == 2){
+            System.out.printf("February\n");
+        }
+         else if(a == 3){
+            System.out.printf("March\n");
+        }
+         else if(a == 4){
+            System.out.printf("April\n");
+        }
+         else if(a == 5){
+            System.out.printf("May\n");
+        }
+         else if(a == 6){
+            System.out.printf("June\n");
+        } else if(a == 7){
+            System.out.printf("July\n");
+        }
+         else if(a == 8){
+            System.out.printf("August\n");
+        }
+         else if(a == 9){
+            System.out.printf("September\n");
+        }
+         else if(a == 10){
+            System.out.printf("October\n");
+        }
+         else if(a == 11){
+            System.out.printf("November\n");
+        }
+         else if(a == 12){
+            System.out.printf("December\n");
+        }
+        
+         
+        
+    }
+    
+}

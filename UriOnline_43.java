5ebec1400098cd304933bd24a88b0f9ae595f03c
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-06-08 22:25:28 +0600

    Uri Online 1064
    Solution of Uri Online 1064, Positives and Average

---
+++ UriOnline_43.java
@@ -0,0 +1,30 @@
+
+package urionline_43;
+
+import java.util.Scanner;
+
+public class UriOnline_43 {
+
+    
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+       int i,c = 0;
+       float avg = 0,s = 0;
+       float [] a = new float[6];
+       for (i = 0; i<6;i++){
+           a[i] = sc.nextFloat();
+            }
+      for (i = 0; i<6;i++){
+           if (a[i]>0){
+               s = (s+a[i]);
+               c++;
+           }
+            }
+      avg = (float) (s/c);
+      System.out.printf("%d valores positivos\n",c);
+      System.out.printf("%.1f\n",avg);
+       
+    }
+    }
+    
+

0b43c6a73e21ed5655748cf28e46981d7ce50330
hmun99 <84393562+hmun99@users.noreply.github.com>
2021-06-08 22:30:01 +0600

    Uri Online 1067
    Solution of Uri Online 1067, Odd Numbers

---
+++ UriOnline_46.java
@@ -0,0 +1,22 @@
+
+package urionline_46;
+
+import java.util.Scanner;
+
+
+public class UriOnline_46 {
+
+ 
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int x = sc.nextInt();
+       int i;
+       for (i = 1; i <= x;i++){
+           if(i%2 != 0){
+               System.out.print(i+"\n");
+                       
+           }
+       }
+    }
+    
+}
